57240ddb6d8662a18a971fff275e949112e9c9b8
Lakshya Kumar <52732477+Lakshya-coder12@users.noreply.github.com>
2021-09-30 19:05:40 +0530

    added HomePage component (#9)

---
+++ communify-react/src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+import React, { FC, memo } from "react";
+
+interface Props {}
+
+const HomePage: FC<Props> = (props) => {
+  return <div></div>;
+};
+
+HomePage.defaultProps = {};
+
+export default memo(HomePage);

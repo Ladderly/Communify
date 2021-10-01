93770a76dbbad817397282d3953875e0069a2912
AyaanJaved <80831951+AyaanJaved@users.noreply.github.com>
2021-10-01 19:06:14 +0530

    updated navbar
    Co-authored-by: Shashank Jain <CodeWithShashankJain@users.noreply.github.com>

--- communify-react/src/App.tsx
+++ communify-react/src/App.tsx
@@ -3,9 +3,9 @@ import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div>
+    <>
       <Navbar />
-    </div>
+    </ >
   );
 }
 
--- communify-react/src/components/Navbar.tsx
+++ communify-react/src/components/Navbar.tsx
@@ -5,42 +5,34 @@ import {FiSearch} from "react-icons/fi"
 import {SiQuora} from "react-icons/si"
 import {MdHome} from "react-icons/md"
 import {BiBookAdd} from "react-icons/bi"
-
 interface Props { }
 
 const navbar: FC<Props> = (props) => {
     return (
-            <nav className="flex px-8 items-center justify-between w-screen h-16 text-center ml-10 mr-10">
-                <div className="flex items-center justify-evenly h-16 w-full">
-                    <div className="flex object-fill ">
-                        <SiQuora className = "w-10 h-8" />
-                    </div>
-                    <div className="flex justify-evenly space-x-10">
-
-                    <div className="flex space-x-8">
-
-                        <li className="list-none  text-xl hover:underline">
-                            <a href="#"><MdHome className="w-7 h-7 bg-gray"/></a>
-                        </li>
-
-                        <li className="list-none hover:underline  text-xl">
-                            <a href="#"><BiBookAdd className="w-7 h-7"/></a>
-                        </li>
+            <nav className="flex h-16 space-x-12 mx-16 items-center">
+                    <div className="flex">
+                        <SiQuora className = "w-12 h-10" />
                     </div>
 
-                    <div className="flex p-3 hover:border-2 border-solid border-secondary-100 border-2 rounded 
+                    <div className="flex justify-between w-full items-center">
+                        <div className="flex space-x-10 ml-10">
+                            <a href="#" className="w-7 h-7 bg-gray"><MdHome className="w-7 h-7"/></a>
+                            <a href="#" className="w-7 h-7"><BiBookAdd className="w-7 h-7"/></a>
+                        </div>
+                    <div className="flex p-3 hover:border-2 border-solid border-2 rounded w-2/4 justify-between
                     ">
-                        <input type="text" className="w-60" placeholder="What are you looking for?"/>
+                        <input type="text" className="w-2/4 focus:outline-none" placeholder="What are you looking for?"/>
                         <button type ="submit" className ="searchButton">
                         <FiSearch className ="w-5 h-5 ml-2"></FiSearch>
                         </button>
                     </div>
-                    <div className="md:flex lg:space-x-10 hidden md:space-x-6">
+                    
+                    <div className="space-x-6">
                         <Button theme="outline">Sign in</Button>
                         <Button>Sign up</Button>
                     </div>
+                    
                     </div>
-                </div>
 
                 {/* <div className="md:hidden">
           <button>

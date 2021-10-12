ac04145c2115242a4a5c294269808efbd77beb61
Lakshya Kumar <lkkumar544@gmail.com>
2021-10-12 19:00:30 +0530

    Add Auth component

---
+++ communify-react/src/pages/Auth/Auth.page.tsx
@@ -0,0 +1,11 @@
+import React, { FC, memo } from "react";
+
+interface Props {}
+
+const Auth: FC<Props> = (props) => {
+  return <div></div>;
+};
+
+Auth.defaultProps = {};
+
+export default memo(Auth);
---
+++ communify-react/src/pages/Auth/SignUp.page.tsx
@@ -0,0 +1,11 @@
+import React, { FC, memo } from "react";
+
+interface Props {}
+
+const SignUp: FC<Props> = (props) => {
+  return <div></div>;
+};
+
+SignUp.defaultProps = {};
+
+export default memo(SignUp);

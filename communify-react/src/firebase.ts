96586ed48d0b827ef2a3bdbad85309d0e38344a9
Lakshya Kumar <lkkumar544@gmail.com>
2021-10-12 18:27:17 +0530

    Add and configured firebase

---
+++ communify-react/src/firebase.ts
@@ -0,0 +1,18 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAGRznM04tdiJgjdvGWdJ3Yt7Q9__ejBzM",
+  authDomain: "communify-b2e04.firebaseapp.com",
+  projectId: "communify-b2e04",
+  storageBucket: "communify-b2e04.appspot.com",
+  messagingSenderId: "56400528497",
+  appId: "1:56400528497:web:8a620dd484018630b2c7d0",
+  measurementId: "G-K8N39BNRHF",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;

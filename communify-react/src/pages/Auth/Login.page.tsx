17bd1626e472215bb548e165aebef3af46a5c36a
Lakshya Kumar <lkkumar544@gmail.com>
2021-10-20 10:56:06 +0530

    fixed Signup and Login issue

--- communify-react/src/pages/Auth/Login.page.tsx
+++ communify-react/src/pages/Auth/Login.page.tsx
@@ -1,11 +1,9 @@
-import React, { FC, memo, useContext, useRef, useState } from "react";
+import React, { FC, memo, useRef, useState } from "react";
 import { useHistory } from "react-router";
 import Button from "../../components/Button";
-import { AuthContext } from "../../context/AuthContext";
 import { auth } from "../../firebase";
-import LoginForm from "../../components/LoginForm";
 
-interface Props { }
+interface Props {}
 
 const Login: FC<Props> = (props) => {
   const [loading, setLoading] = useState(false);
@@ -53,25 +51,55 @@ const Login: FC<Props> = (props) => {
     });
     return promise;
   };
- 
-  return (
-    <div className="flex flex-col w-full min-h-screen justify-center bg-login bg-no-repeat  bg-fill">
-      <div className="flex bg-white flex-col sm:justify-center sm:w-4/6 w-3/5 h-5/6 mx-auto space-y-2 p-1.5">
 
-        <div className="flex h-3/6 justify-center">
-          <h1 className="sm:text-5xl text-3xl text-center">QUORA</h1>
+  return (
+    <div className="flex flex-col justify-center w-full min-h-screen bg-no-repeat bg-login bg-fill">
+      <div className="flex bg-white flex-col sm:justify-center sm:w-4/6 w-3/5 h-5/6 mx-auto space-y-2 p-1.5">
+        <div className="flex justify-center h-3/6">
+          <h1 className="text-3xl text-center sm:text-5xl">QUORA</h1>
         </div>
 
         <div className="flex h-80 justify-center p-1 space-x-0.5">
           {/* continue with*/}
-          <div className=" invisible sm:visible p-1 sm:w-4/5"></div>
+          <div className="invisible p-1 sm:visible sm:w-4/5"></div>
+          {/* border */}{" "}
+          <div className="invisible sm:border-2 sm:visible"></div>
+          <div className="w-4/5 p-1 sm:w-4/5">
+            <form
+              onSubmit={(e) => handleSignIn(e)}
+              action=""
+              className="flex flex-col space-y-4 "
+            >
+              <div className="text-xl sm:text-center sm:text-3xl">Login</div>
 
-          {/* border */} <div className="sm:border-2 invisible sm:visible"></div>
-          
-          <div className="sm:w-4/5 w-4/5 p-1">
-            <LoginForm />
+              <div className="flex flex-col space-y-2">
+                <label htmlFor="email" className="text-lg">
+                  Email
+                </label>
+                <input
+                  ref={emailRef}
+                  className="border-2 border-transparent border-solid rounded-full focus:outline-none hover:border-2 focus:ring-2 focus:ring-secondary-200 focus:border-transparent"
+                  type="text"
+                  name="email"
+                  autoComplete="off"
+                />
+              </div>
+
+              <div className="flex flex-col space-y-2 ">
+                <label htmlFor="email" className="text-lg">
+                  Password
+                </label>
+                <input
+                  ref={passwordRef}
+                  className="border-2 border-transparent border-solid rounded-full focus:outline-none hover:border-2 focus:ring-2 focus:ring-secondary-200 focus:border-transparent"
+                  type="password"
+                  name="password"
+                />
+              </div>
+
+              <Button className="w-20">Login</Button>
+            </form>
           </div>
-
         </div>
       </div>
     </div>

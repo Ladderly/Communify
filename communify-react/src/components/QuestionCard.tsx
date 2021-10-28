1e4dc7a3c7abe0c1116f8ec2b7895fad3f0ad2a2
Lakshya Kumar <lkkumar544@gmail.com>
2021-10-29 02:19:17 +0530

    Add tag in question card

--- communify-react/src/components/QuestionCard.tsx
+++ communify-react/src/components/QuestionCard.tsx
@@ -2,12 +2,14 @@ import React, { FC, memo } from "react";
 import { IoMdClose, IoMdShareAlt } from "react-icons/io";
 import { useHistory } from "react-router";
 import Button from "./Button";
+import { AiFillTags } from "react-icons/ai";
 
 interface Props {
   children: string;
+  tag: string;
 }
 
-const QuestionCard: FC<Props> = ({ children }) => {
+const QuestionCard: FC<Props> = ({ children, tag }) => {
   const history = useHistory();
   return (
     <div className="relative w-2/5 mx-auto mt-4 bg-gray-100 shadow-lg">
@@ -23,11 +25,16 @@ const QuestionCard: FC<Props> = ({ children }) => {
         <button>
           <IoMdClose className="absolute w-6 h-6 rounded-full cursor-pointer right-2 top-2 text-secondary-400 hover:bg-gray-300" />
         </button>
-        <div className="mt-8 border-b-2 border-secondary-400"></div>
+        <div className="flex items-center mt-4 space-x-2">
+          <AiFillTags className="w-6 h-6 text-secondary-400" />
+          <p className="text-sm font-semibold text-secondary-400">{tag}</p>
+        </div>
+
+        <div className="mt-4 border-b-2 border-secondary-400"></div>
         <div className="flex items-center justify-between py-2 mx-6">
           <Button>Answer</Button>
           <button>
-            <IoMdShareAlt className="w-8 h-8 rounded-full text-secondary-300 hover:bg-gray-300" />
+            <IoMdShareAlt className="w-8 h-8 rounded-full text-secondary-400 hover:bg-gray-300" />
           </button>
         </div>
       </div>

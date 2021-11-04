ddc55617b2f53cc074900e53e70d4b59f4e8aa0b
Lakshya Kumar <lkkumar544@gmail.com>
2021-11-04 21:39:21 +0530

    Add feature to only show questions not asked by signed in user

--- communify-react/src/pages/AppContainer/QuestionList.page.tsx
+++ communify-react/src/pages/AppContainer/QuestionList.page.tsx
@@ -1,26 +1,36 @@
 import firebase from "firebase/compat";
-import React, { FC, memo, useEffect, useState } from "react";
+import React, { FC, memo, useContext, useEffect, useState } from "react";
 import QuestionCard from "../../components/QuestionCard";
+import { AuthContext } from "../../context/AuthContext";
 import { firestore } from "../../firebase";
 
 interface Props {}
 
 const QuestionList: FC<Props> = (props) => {
+  const user = useContext(AuthContext);
+  if (!sessionStorage.getItem("user")) {
+    sessionStorage.setItem("user", user!.uid);
+  }
   const [questionData, setQuestionData] = useState<
     firebase.firestore.DocumentData[]
   >([]);
   useEffect(() => {
-    firestore
-      .collection("questions")
-      .get()
-      .then((questionList) => {
-        questionList.docs.forEach((question) => {
-          setQuestionData((prev) => [...prev, question.data()]);
+    const fetchList = async () => {
+      await firestore
+        .collection("questions")
+        .where("uid", "!=", sessionStorage.getItem("user")!)
+        .get()
+        .then((questionList) => {
+          questionList.docs.forEach((question) => {
+            setQuestionData((prev) => [...prev, question.data()]);
+          });
+        })
+        .catch((error) => {
+          console.log(error);
         });
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    };
+    fetchList();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return (
     <div>

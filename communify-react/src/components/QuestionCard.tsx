026c9cdaf80eac330f236f286f436f01f82cf4ef
Lakshya Kumar <lkkumar544@gmail.com>
2021-11-06 15:26:12 +0530

    Customized for profile page

--- communify-react/src/components/QuestionCard.tsx
+++ communify-react/src/components/QuestionCard.tsx
@@ -3,32 +3,42 @@ import { IoMdClose, IoMdShareAlt } from "react-icons/io";
 import { useHistory } from "react-router";
 import Button from "./Button";
 import { AiFillTags } from "react-icons/ai";
+import { MdDelete } from "react-icons/md";
 
 interface Props {
   children: string;
   questionID: string;
   tag: string;
+  deleted?: boolean;
 }
 
-const QuestionCard: FC<Props> = ({ children, tag, questionID }) => {
+const QuestionCard: FC<Props> = ({ children, tag, questionID, deleted }) => {
   const history = useHistory();
   const [visibility, setVisibility] = useState<boolean>(true);
   const [undoVisibility, setUndoVisibility] = useState<boolean>(false);
   return (
     <>
-      <div className="w-full px-2 sm:w-2/5 sm:px-0 sm:mx-auto">
+      <div
+        className={
+          !deleted ? "w-full px-2 sm:w-2/5 sm:px-0 sm:mx-auto" : "w-full"
+        }
+      >
         <div className="relative mt-4 bg-gray-100 shadow-lg">
           {visibility ? (
             <div className="px-5 pt-3">
               <h2 className="text-lg font-semibold">{children}</h2>
               <button>
-                <IoMdClose
-                  onClick={() => {
-                    setVisibility(false);
-                    setUndoVisibility(true);
-                  }}
-                  className="absolute w-6 h-6 cursor-pointer right-2 top-2 text-secondary-400"
-                />
+                {!deleted ? (
+                  <IoMdClose
+                    onClick={() => {
+                      setVisibility(false);
+                      setUndoVisibility(true);
+                    }}
+                    className="absolute w-6 h-6 cursor-pointer right-2 top-2 text-secondary-400"
+                  />
+                ) : (
+                  <MdDelete className="absolute w-6 h-6 text-red-500 cursor-pointer right-2 top-2" />
+                )}
               </button>
               <div className="flex items-center mt-4 space-x-2">
                 <AiFillTags className="w-6 h-6 text-secondary-400" />
@@ -70,6 +80,8 @@ const QuestionCard: FC<Props> = ({ children, tag, questionID }) => {
   );
 };
 
-QuestionCard.defaultProps = {};
+QuestionCard.defaultProps = {
+  deleted: false,
+};
 
 export default memo(QuestionCard);

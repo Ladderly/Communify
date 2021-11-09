084292a5e42ce1158be56b5f5a70e51963084d98
Lakshya Kumar <lkkumar544@gmail.com>
2021-11-10 01:39:32 +0530

    Add feature to delete questions and answers from profile

--- communify-react/src/components/ProfileAnswerCard.tsx
+++ communify-react/src/components/ProfileAnswerCard.tsx
@@ -1,15 +1,19 @@
-import React, { FC, memo, useEffect, useState } from "react";
+import React, { FC, memo, useContext, useEffect, useState } from "react";
 import { MdDelete } from "react-icons/md";
 import firebase from "firebase/compat/app";
-import { firestore } from "../firebase";
+import { firestore, storage } from "../firebase";
 import { useHistory } from "react-router";
 import Button from "./Button";
+import { useParams } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
 
 interface Props {
   answer: string;
   imgSrc?: string;
   created: firebase.firestore.Timestamp;
   questionID: string;
+  answerID: string;
+  fileName: string;
 }
 
 const ProfileAnswerCard: FC<Props> = ({
@@ -17,8 +21,16 @@ const ProfileAnswerCard: FC<Props> = ({
   imgSrc,
   created,
   questionID,
+  answerID,
+  fileName,
 }) => {
   const history = useHistory();
+  const { userID } = useParams<any>();
+  const [loading, setLoading] = useState<boolean>(false);
+  const user = useContext(AuthContext);
+  if (!sessionStorage.getItem("user") && user) {
+    sessionStorage.setItem("user", user!.uid);
+  }
   const [question, setQuestion] = useState<string>("");
   const [deleteModel, setDeleteModel] = useState<boolean>(false);
   useEffect(() => {
@@ -38,6 +50,36 @@ const ProfileAnswerCard: FC<Props> = ({
     };
     findQuestion(); //eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+  const deleteAnswer = async () => {
+    try {
+      await firestore
+        .collection("answers")
+        .doc(answerID)
+        .delete()
+        .then(() => {
+          firestore
+            .collection("questions")
+            .doc(questionID)
+            .update({
+              numberOfAnswers: firebase.firestore.FieldValue.increment(-1),
+            })
+            .then(() => {
+              storage
+                .ref("files")
+                .child(fileName)
+                .delete()
+                .then(() => window.location.reload())
+                .catch((err) => {
+                  setLoading(false);
+                  window.location.reload();
+                  console.log(err);
+                });
+            });
+        });
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <div className="relative w-full p-5 mt-4 bg-gray-100 shadow-md">
       {!deleteModel ? (
@@ -52,12 +94,14 @@ const ProfileAnswerCard: FC<Props> = ({
             {question}
           </p>
           <p className="mt-6">{answer}</p>
-          <button>
-            <MdDelete
-              onClick={() => setDeleteModel(true)}
-              className="absolute w-6 h-6 text-red-500 cursor-pointer right-2 top-2"
-            />
-          </button>
+          {userID === sessionStorage.getItem("user")! && (
+            <button>
+              <MdDelete
+                onClick={() => setDeleteModel(true)}
+                className="absolute w-6 h-6 text-red-500 cursor-pointer right-2 top-2"
+              />
+            </button>
+          )}
           {imgSrc && (
             <img
               className="object-contain w-full h-full mt-4"
@@ -73,7 +117,15 @@ const ProfileAnswerCard: FC<Props> = ({
           </p>
           <div className="flex items-center justify-center mt-6 space-x-8">
             <Button onClick={() => setDeleteModel(false)}>Undo</Button>
-            <Button theme="warning" onClick={() => setDeleteModel(false)}>
+            <Button
+              className="w-20"
+              loading={loading}
+              theme="warning"
+              onClick={() => {
+                setLoading(true);
+                deleteAnswer();
+              }}
+            >
               Delete
             </Button>
           </div>
